9217616f289ed6b15c350558e18c6addcc462a9e
Ahmed Harbi <ahmed.harbi.eg@gmail.com>
2025-05-04 23:40:34 +0300

    updating chrome github actions

--- features/environment.py
+++ features/environment.py
@@ -1,8 +1,19 @@
+import chromedriver_autoinstaller
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 
 def before_all(context):
-    context.browser = webdriver.Chrome()  # Open a Chrome browser
-    context.browser.implicitly_wait(1)   # Wait up to 10 seconds for elements to appear
+    # Automatically downloads and installs correct ChromeDriver version
+    chromedriver_autoinstaller.install()
+
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
+    context.browser = webdriver.Chrome(options=options)
+
 
 def after_all(context):
-    context.browser.quit()  # Close browser after all tests
+    context.browser.quit()
